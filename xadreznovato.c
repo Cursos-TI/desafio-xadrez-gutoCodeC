f24f23d019440c00098a4d7cb485fc5c4d6efb59
gutoCodeC <luisgscode@gmail.com>
2025-07-14 11:58:55 +0000

    criação do xadrez

--- xadreznovato.c
+++ xadreznovato.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
-// Constantes para os movimentos de cada peça
+
 #define MOV_BISPO 5
 #define MOV_TORRE 5
 #define MOV_RAINHA 8
 
 int main() {
-    // ============================
-    // MOVIMENTO DO BISPO (FOR)
-    // Diagonal Superior Direita = combinação de "Cima" e "Direita"
-    // ============================
+   
     printf("Movimentação do Bispo:\n");
     for (int i = 0; i < MOV_BISPO; i++) {
         printf("Cima\n");
@@ -17,10 +14,7 @@ int main() {
     }
     printf("\n");
 
-    // ============================
-    // MOVIMENTO DA TORRE (WHILE)
-    // Direita 5 vezes
-    // ============================
+    
     printf("Movimentação da Torre:\n");
     int j = 0;
     while (j < MOV_TORRE) {
@@ -29,10 +23,8 @@ int main() {
     }
     printf("\n");
 
-    // ============================
-    // MOVIMENTO DA RAINHA (DO-WHILE)
-    // Esquerda 8 vezes
-    // ============================
+    /
+   
     printf("Movimentação da Rainha:\n");
     int k = 0;
     do {
